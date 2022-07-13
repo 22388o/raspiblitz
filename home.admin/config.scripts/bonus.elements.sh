bf715d2203792591ca4409a08ae34c7ee2857aef
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2022-07-13 09:58:40 -0300

    Update bonus.elements.sh

--- home.admin/config.scripts/bonus.elements.sh
+++ home.admin/config.scripts/bonus.elements.sh
@@ -20,7 +20,7 @@ fi
 # command info
 if [ $# -eq 0 ] || [ "$1" = "-h" ] || [ "$1" = "-help" ]; then
  echo " Elments install script"
- echo "/home/admin/config.scripts/bonus.chantools.sh on|off|menu"
+ echo "/home/admin/config.scripts/bonus.elements.sh on|off|menu"
  echo "Installs the version $pinnedVersion by default."
  exit 1
 fi
@@ -32,8 +32,8 @@ fi
 
 # show info menu
 if [ "$1" = "menu" ]; then
-  dialog --title " Channel Tools ${pinnedVersion} " --msgbox "\n
-Channel Tools is a command line tool to rescue locked funds.\n\n
+  dialog --title " Elements ${pinnedVersion} " --msgbox "\n
+ELements is a command line tool to rescue locked funds.\n\n
 On terminal use command 'chantools' and follow instructions.\n\n
 Usage: https://github.com/guggero/chantools/blob/master/README.md
 " 11 75
@@ -76,7 +76,7 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   fi
   SHA256=$(grep -i "linux-$OSversion" manifest-v$pinnedVersion.txt | cut -d " " -f1)
   echo 
-  echo "# Channel Tools v${pinnedVersion} for ${OSversion}"
+  echo "# Elements v${pinnedVersion} for ${OSversion}"
   echo "# SHA256 hash: $SHA256"
   echo 
 
@@ -89,7 +89,7 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   sudo -u admin wget --no-check-certificate -N -O "${downloadDir}/pgp_keys.asc" ${PGPpkeys}
   binaryChecksum=$(sha256sum ${binaryName} | cut -d " " -f1)
   if [ "${binaryChecksum}" != "${SHA256}" ]; then
-    echo "# !!! FAIL !!! Downloaded Channel Tools BINARY not matching SHA256 checksum: ${SHA256}"
+    echo "# !!! FAIL !!! Downloaded Elements BINARY not matching SHA256 checksum: ${SHA256}"
     exit 1
   fi
 
@@ -98,7 +98,7 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   fingerprint=$(sudo gpg "${downloadDir}/pgp_keys.asc" 2>/dev/null | grep "${PGPcheck}" -c)
   if [ ${fingerprint} -lt 1 ]; then
     echo
-    echo "# !!! BUILD WARNING --> Channel Tools PGP author not as expected"
+    echo "# !!! BUILD WARNING --> Elements PGP author not as expected"
     echo "# Should contain PGP: ${PGPcheck}"
     echo "# PRESS ENTER to TAKE THE RISK if you think all is OK"
     read key
@@ -121,9 +121,9 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   # install
   sudo -u admin tar -xzf ${binaryName}
   sudo -u bitcoin mkdir -p /home/bitcoin/bin 2>/dev/null
-  sudo install -m 0755 -o bitcoin -g bitcoin -t /home/bitcoin/bin chantools-linux-${OSversion}-v${pinnedVersion}/*
+  sudo install -m 0755 -o bitcoin -g bitcoin -t /home/bitcoin/bin elements-linux-${OSversion}-v${pinnedVersion}/*
   sleep 3
-  installed=$(sudo -u bitcoin /home/bitcoin/bin/chantools --version)
+  installed=$(sudo -u bitcoin /home/bitcoin/bin/elements --version)
   if [ ${#installed} -eq 0 ]; then
     echo
     echo "# !!! BUILD FAILED --> Was not able to install Channel Tools"
@@ -135,7 +135,7 @@ if [ "$1" = "1" ] || [ "$1" = "on" ]; then
   echo
   echo "Installed ${installed}"
   echo "
-# Channel Tools is a command line tool.
+# Elements is a command line tool.
 # Type: 'sudo su - bitcoin' in the command line to switch to the bitcoin user.
 # Then see 'chantools' for the options.
 # Usage: https://github.com/guggero/chantools/blob/master/README.md
@@ -147,12 +147,12 @@ fi
 if [ "$1" = "0" ] || [ "$1" = "off" ]; then
 
   # setting value in raspi blitz config
-  sudo sed -i "s/^chantools=.*/chantools=off/g" /mnt/hdd/raspiblitz.conf
+  sudo sed -i "s/^chantools=.*/elements=off/g" /mnt/hdd/raspiblitz.conf
   
-  echo "# REMOVING Channel Tools"
-  sudo rm -rf /home/admin/download/chantools*
-  sudo rm -rf /home/bitcoin/bin/chantools*
-  echo "# OK, chantools is removed."
+  echo "# REMOVING Elemens"
+  sudo rm -rf /home/admin/download/elements*
+  sudo rm -rf /home/bitcoin/bin/elements*
+  echo "# OK, elements is removed."
   exit 0
 
 fi
